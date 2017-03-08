e71b4bf694fd66d8cdb1c8d1738fd5a8f3ccffc4
Eric Karge <eric.karge@meteogroup.com>
2017-03-08 18:00:19 +0100

    Squash docker image

--- travis/build-and-test.sh
+++ travis/build-and-test.sh
@@ -10,5 +10,5 @@ ln amqp-to-kafka-linux64 amqp-to-kafka
 GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -o amqp-to-kafka-darwin64
 GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o amqp-to-kafka-windows64.exe
 
-docker build -t "meteogroup/amqp-to-kafka:$COMMIT" .
+docker build --no-cache --pull --squash -t "meteogroup/amqp-to-kafka:$COMMIT" .
 test/test.sh "$COMMIT"
