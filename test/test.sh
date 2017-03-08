161c6cc5367b6535287771ca641152312be1d80b
Eric Karge <eric.karge@meteogroup.com>
2017-03-08 17:51:38 +0100

    Use docker health checks to coordinate test setup

--- test/test.sh
+++ test/test.sh
@@ -14,13 +14,6 @@ main() {
 }
 
 setup() {
-  local i
-  docker-compose -p "$TEST_STAGE" up -d kafka amqp
-
-  wait-for kafka 9092
-  wait-for amqp 5672
-
-  in-test-stage nasqueron/rabbitmqadmin declare exchange name=public type=topic --host=amqp
   docker-compose -p "$TEST_STAGE" up -d amqp-to-kafka
 }
 
@@ -32,17 +25,6 @@ tear-down() {
   fi
   docker-compose -p "$TEST_STAGE" down --rmi local --volumes --remove-orphans
   return "$result"
-}
-
-wait-for() {
-  local service="$1" port="$2"
-  for ((i = 0; i < 10; ++i)); do
-    in-test-stage toolbelt/netcat -z "$service" "$port" && break
-  done
-}
-
-in-test-stage() {
-  docker run --rm --net "${TEST_STAGE}_default" "$@"
 }
 
 main "$@"
